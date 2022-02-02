9c205c19ab1d51a5764374983efe7d09e69ccea7
Steven Du <sdu@yugabyte.com>
2022-02-02 15:03:41 -0500

    implement create provider resource

---
+++ examples/resources/cloud_provider_resource/resource.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    yb = {
+      version = "~> 0.1.0"
+      source = "terraform.yugabyte.com/platform/yugabyte-platform"
+    }
+  }
+}
+
+provider "yb" {
+  apikey = "***REMOVED***"
+  host = "http://portal.dev.yugabyte.com"
+}
+
+data "yb_customer" "customer" {}
+
+resource "yb_cloud_provider" "gcp" {
+  customer_id = data.yb_customer.customer.id
+  code = "gcp"
+  config = {
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+    YB_FIREWALL_TAGS = "cluster-server"
+  }
+  dest_vpc_id = "***REMOVED***"
+  name = "my-gcp-provider"
+  regions {
+    code = "us-central1"
+    name = "us-central1"
+  }
+  ssh_port = 54422
+  air_gap_install = false
+}
+
+output "provider_uuid" {
+  value = yb_cloud_provider.gcp.id
+}
+
+output "task_uuid" {
+  value = yb_cloud_provider.gcp.task_uuid
+}
