77d1c13b272c10f29eb6b431e2f03e5c01f12ac1
Steven Du <sdu@yugabyte.com>
2022-02-02 15:03:41 -0500

    implement create provider resource

--- internal/provider/data_source_customer.go
+++ internal/provider/data_source_customer.go
@@ -3,29 +3,18 @@ package provider
 import (
 	"context"
 	"encoding/json"
-	"net/http"
-	"strconv"
-	"time"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"net/http"
 )
 
 func dataSourceCustomer() *schema.Resource {
 	return &schema.Resource{
-		// This description is used by the documentation generator and the language server.
 		Description: "Retrieve customer UUID",
 
 		ReadContext: dataSourceCustomerRead,
 
-		Schema: map[string]*schema.Schema{
-			"uuid": {
-				// This description is used by the documentation generator and the language server.
-				Description: "Customer UUID",
-				Type:        schema.TypeString,
-				Computed:    true,
-			},
-		},
+		Schema: map[string]*schema.Schema{},
 	}
 }
 
@@ -40,16 +29,11 @@ func dataSourceCustomerRead(ctx context.Context, d *schema.ResourceData, meta in
 	defer r.Body.Close()
 
 	customer := make(map[string]interface{})
-	err = json.NewDecoder(r.Body).Decode(&customer)
-	if err != nil {
+	if err = json.NewDecoder(r.Body).Decode(&customer); err != nil {
 		return diag.FromErr(err)
 	}
 
-	if err := d.Set("uuid", customer["customerUUID"]); err != nil {
-		return diag.FromErr(err)
-	}
-
-	d.SetId(strconv.FormatInt(time.Now().Unix(), 10))
+	d.SetId(customer["customerUUID"].(string))
 
 	return diags
 }
---
+++ internal/provider/resource_cloud_provider.go
@@ -0,0 +1,286 @@
+package provider
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	utils "github.com/yugabyte/terraform-provider-yugabyte-platform/internal"
+	"net/http"
+)
+
+func resourceCloudProvider() *schema.Resource {
+	return &schema.Resource{
+		Description: "Cloud Provider Resource",
+
+		CreateContext: resourceCloudProviderCreate,
+		ReadContext:   resourceCloudProviderRead,
+		UpdateContext: resourceCloudProviderUpdate,
+		DeleteContext: resourceCloudProviderDelete,
+
+		Schema: map[string]*schema.Schema{
+			"customer_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"air_gap_install": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"code": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"config": {
+				Type:     schema.TypeMap,
+				Elem:     schema.TypeString,
+				Optional: true,
+			},
+			"custom_host_cidrs": {
+				Type: schema.TypeList,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+				Optional: true,
+				ForceNew: true,
+			},
+			"dest_vpc_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"host_vpc_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"host_vpc_region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"hosted_zone_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"hosted_zone_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"key_pair_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"regions": {
+				Type:     schema.TypeList,
+				Required: true,
+				ForceNew: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"config": {
+							Type:     schema.TypeMap,
+							Elem:     schema.TypeString,
+							Optional: true,
+						},
+						"name": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"security_group_id": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"vnet_name": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"yb_image": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"code": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"zones": {
+							Type:     schema.TypeList,
+							Optional: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"code": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"config": {
+										Type:     schema.TypeMap,
+										Elem:     schema.TypeString,
+										Optional: true,
+									},
+									"name": {
+										Type:     schema.TypeString,
+										Required: true,
+									},
+									"secondary_subnet": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+									"subnet": {
+										Type:     schema.TypeString,
+										Optional: true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			"ssh_port": {
+				Type:     schema.TypeInt,
+				Optional: true,
+				ForceNew: true,
+			},
+			"ssh_private_key_content": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"ssh_user": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"task_uuid": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceCloudProviderCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	p := buildCloudProvider(d)
+	body, err := json.Marshal(p)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	c := meta.(*ApiClient)
+	cUUID := d.Get("customer_id").(string)
+	r, err := c.MakeRequest(http.MethodPost, fmt.Sprintf("api/v1/customers/%s/providers", cUUID), bytes.NewBuffer(body))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	defer r.Body.Close()
+
+	res := make(map[string]interface{})
+	if err = json.NewDecoder(r.Body).Decode(&res); err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(res["resourceUUID"].(string))
+	if err = d.Set("task_uuid", res["taskUUID"].(string)); err != nil {
+		return diag.FromErr(err)
+	}
+	return diags
+}
+
+func buildCloudProvider(d *schema.ResourceData) map[string]interface{} {
+	p := make(map[string]interface{})
+
+	utils.ResourceSetIfExists(p, d, "air_gap_install", "airGapInstall")
+	utils.ResourceSetIfExists(p, d, "code", "code")
+	utils.ResourceSetIfExists(p, d, "custom_host_cidrs", "customHostCidrs")
+	utils.ResourceSetIfExists(p, d, "dest_vpc_id", "destVpcId")
+	utils.ResourceSetIfExists(p, d, "host_vpc_id", "hostVpcId")
+	utils.ResourceSetIfExists(p, d, "host_vpc_region", "hostVpcRegion")
+	utils.ResourceSetIfExists(p, d, "hosted_zone_id", "hostedZoneId")
+	utils.ResourceSetIfExists(p, d, "hosted_zone_name", "hostedZoneName")
+	utils.ResourceSetIfExists(p, d, "key_pair_name", "keyPairName")
+	utils.ResourceSetIfExists(p, d, "name", "name")
+	utils.ResourceSetIfExists(p, d, "ssh_port", "sshPort")
+	utils.ResourceSetIfExists(p, d, "ssh_private_key_content", "sshPrivateKeyContent")
+	utils.ResourceSetIfExists(p, d, "ssh_user", "sshUser")
+
+	if v, exists := d.GetOk("regions"); exists {
+		p["regions"] = buildRegions(v.([]interface{}))
+	}
+
+	return p
+}
+
+func buildRegions(regions []interface{}) []map[string]interface{} {
+	var res []map[string]interface{}
+
+	for _, v := range regions {
+		region := v.(map[string]interface{})
+		r := make(map[string]interface{})
+
+		utils.MapSetIfExists(r, region, "config", "config")
+		utils.MapSetIfExists(r, region, "name", "name")
+		utils.MapSetIfExists(r, region, "security_group_id", "securityGroupId")
+		utils.MapSetIfExists(r, region, "vnet_name", "vnetName")
+		utils.MapSetIfExists(r, region, "yb_image", "ybImage")
+		utils.MapSetIfExists(r, region, "code", "code")
+
+		if w, exists := region["zones"]; exists {
+			r["zones"] = buildZones(w.([]interface{}))
+		} else {
+			r["zones"] = []interface{}{}
+		}
+	}
+
+	return res
+}
+
+func buildZones(zones []interface{}) []map[string]interface{} {
+	var res []map[string]interface{}
+
+	for _, v := range zones {
+		zone := v.(map[string]interface{})
+		z := make(map[string]interface{})
+
+		utils.MapSetIfExists(z, zone, "code", "code")
+		utils.MapSetIfExists(z, zone, "config", "config")
+		utils.MapSetIfExists(z, zone, "name", "name")
+		utils.MapSetIfExists(z, zone, "secondary_subnet", "secondarySubnet")
+		utils.MapSetIfExists(z, zone, "subnet", "subnet")
+
+		res = append(res, z)
+	}
+
+	return res
+}
+
+func resourceCloudProviderRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	// use the meta value to retrieve your client from the provider configure method
+	// client := meta.(*apiClient)
+	return diag.Errorf("not implemented")
+}
+
+func resourceCloudProviderUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	// use the meta value to retrieve your client from the provider configure method
+	// client := meta.(*apiClient)
+
+	return diag.Errorf("not implemented")
+}
+
+func resourceCloudProviderDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	// use the meta value to retrieve your client from the provider configure method
+	// client := meta.(*apiClient)
+
+	return diag.Errorf("not implemented")
+}
