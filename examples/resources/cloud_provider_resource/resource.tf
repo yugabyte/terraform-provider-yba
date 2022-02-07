4eb6e98d34fca8f25a60a541d2a491dd8db6f292
Steven Du <sdu@yugabyte.com>
2022-02-07 18:49:22 -0500

    add provider access key data source

--- examples/resources/cloud_provider_resource/resource.tf
+++ examples/resources/cloud_provider_resource/resource.tf
@@ -37,6 +37,35 @@ resource "yb_cloud_provider" "gcp" {
   air_gap_install = false
 }
 
+data "yb_provider_key" "gcp-key" {}
+
+resource "yb_universe" "gcp_universe" {
+  clusters {
+    cluster_type = "PRIMARY"
+    user_intent {
+      universe_name = "sdu-test-gcp-universe"
+      provider_type = "aws"
+      provider = yb_cloud_provider.gcp.id
+      region_list = [for r in yb_cloud_provider.gcp.regions : r.uuid]
+      num_nodes = 3
+      replication_factor = 3
+      instance_type = "c5.large"
+      device_info {
+        num_volumes = 1
+        volume_size = 250
+        storage_type = "GP2"
+      }
+      assign_public_ip = true
+      use_time_sync = true
+      enable_ysql = true
+      enable_node_to_node_encrypt = true
+      enable_client_to_node_encrypt = true
+      yb_software_version = "2.7.3.0-b80"
+      access_key_code = data.yb_provider_key.gcp-key.id
+    }
+  }
+}
+
 output "provider" {
   value = yb_cloud_provider.gcp
 }
