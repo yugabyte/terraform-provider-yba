f95c593fc0cf177fe79f192a174dae6224a9a38c
Steven Du <sdu@yugabyte.com>
2022-02-08 18:34:37 -0500

    fixes for types

--- examples/resources/resource.tf
+++ examples/resources/resource.tf
@@ -14,6 +14,7 @@ provider "yb" {
 
 resource "yb_cloud_provider" "gcp" {
   code = "gcp"
+  custom_host_cidrs = []
   config = {
     ***REMOVED***
     ***REMOVED***
@@ -51,32 +52,32 @@ output "region_list" {
   value = local.region_list
 }
 
-resource "yb_universe" "gcp_universe" {
-  depends_on = [yb_cloud_provider.gcp]
-  clusters {
-    cluster_type = "PRIMARY"
-    user_intent {
-      universe_name = "sdu-test-gcp-universe"
-      provider_type = "gcp"
-      provider = local.provider_id
-      region_list = local.region_list
-      num_nodes = 3
-      replication_factor = 3
-      instance_type = "n1-standard-1"
-      device_info {
-        num_volumes = 1
-        volume_size = 375
-        storage_type = "Persistent"
-      }
-      assign_public_ip = true
-      use_time_sync = true
-      enable_ysql = true
-      enable_node_to_node_encrypt = true
-      enable_client_to_node_encrypt = true
-      yb_software_version = "2.7.3.0-b80"
-      access_key_code = local.provider_key
-    }
-  }
-}
+#resource "yb_universe" "gcp_universe" {
+#  depends_on = [yb_cloud_provider.gcp]
+#  clusters {
+#    cluster_type = "PRIMARY"
+#    user_intent {
+#      universe_name = "sdu-test-gcp-universe"
+#      provider_type = "gcp"
+#      provider = local.provider_id
+#      region_list = local.region_list
+#      num_nodes = 3
+#      replication_factor = 3
+#      instance_type = "n1-standard-1"
+#      device_info {
+#        num_volumes = 1
+#        volume_size = 375
+#        storage_type = "Persistent"
+#      }
+#      assign_public_ip = true
+#      use_time_sync = true
+#      enable_ysql = true
+#      enable_node_to_node_encrypt = true
+#      enable_client_to_node_encrypt = true
+#      yb_software_version = "2.7.3.0-b80"
+#      access_key_code = local.provider_key
+#    }
+#  }
+#}
 
 
