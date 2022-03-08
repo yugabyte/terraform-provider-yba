930c3aab9faf3634b2bd00d5226ebefdacde3865
Steven Du <sdu@yugabyte.com>
2022-03-07 19:02:43 -0500

    reformat

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ import (
 var (
 	// these will be set by the goreleaser configuration
 	// to appropriate values for the compiled binary
-	version string = "dev"
+	version = "dev"
 
 	// goreleaser can also pass the specific commit if you want
 	// commit  string = ""
