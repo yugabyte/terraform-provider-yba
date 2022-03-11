946c2d5813ef48f701c7548c7709c179f4f99050
Steven Du <sdu@yugabyte.com>
2022-03-11 12:39:47 -0500

    remove sensitive values from config

--- examples/test/main.tf
+++ examples/test/main.tf
@@ -8,23 +8,31 @@ terraform {
 }
 
 data "yb_customer_data" "customer" {
-  api_token = "7b0138b7-df1e-42ca-9941-2edfc15dca96"
+  api_token = "c611f38b-3ea1-4587-8cad-fa041f55b11a"
 }
 
 provider "yb" {
   // these can be set as environment variables
-  host = "35.203.183.215:80"
+  host = "portal.dev.yugabyte.com"
 }
 
-resource "yb_storage_config_resource" "config" {
+resource "yb_cloud_provider" "gcp" {
   connection_info {
     cuuid     = data.yb_customer_data.customer.cuuid
     api_token = data.yb_customer_data.customer.api_token
   }
 
-  config_name = "hi"
-  data = {
-    random = "hi"
+  code = "gcp"
+  config = merge(
+    { YB_FIREWALL_TAGS = "cluster-server" },
+    jsondecode(file("/Users/stevendu/.yugabyte/yugabyte-gce.json"))
+  )
+  dest_vpc_id = "yugabyte-network"
+  name        = "sdu-test-gcp-provider"
+  regions {
+    code = "us-west1"
+    name = "us-west1"
   }
-  name = "bye"
+  ssh_port        = 54422
+  air_gap_install = false
 }
