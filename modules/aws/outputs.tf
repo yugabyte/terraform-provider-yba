fa5dca9e7dd1558f01d75e062f6089e0af19f526
Steven Du <sdu@yugabyte.com>
2022-03-18 13:36:03 -0400

    add private and public ip flag to installation command

--- modules/aws/outputs.tf
+++ modules/aws/outputs.tf
@@ -1,3 +1,7 @@
 output "public_ip" {
   value = aws_instance.yb_platform_node.public_ip
 }
+
+output "private_ip" {
+  value = aws_instance.yb_platform_node.private_ip
+}
--- modules/azure/outputs.tf
+++ modules/azure/outputs.tf
@@ -1,3 +1,7 @@
 output "public_ip" {
   value = azurerm_public_ip.yb_public_ip.ip_address
 }
+
+output "private_ip" {
+  value = azurerm_network_interface.yb_network_interface.private_ip_address
+}
