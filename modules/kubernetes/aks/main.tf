7f0b8d455fdd78d3a6831c894a2f9491e2029ce6
Steven Du <sdu@yugabyte.com>
2022-03-23 14:15:49 -0400

    add aks module

---
+++ modules/kubernetes/aks/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = ">= 2.0.3"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.1.0"
+    }
+  }
+}
+
+data "azurerm_kubernetes_cluster" "yb-anywhere" {
+  depends_on          = [module.aks-cluster]
+  name                = var.cluster_name
+  resource_group_name = var.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.azurerm_kubernetes_cluster.yb-anywhere.kube_config.0.host
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.yb-anywhere.kube_config.0.client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.yb-anywhere.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.yb-anywhere.kube_config.0.cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.azurerm_kubernetes_cluster.yb-anywhere.kube_config.0.host
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.yb-anywhere.kube_config.0.client_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.yb-anywhere.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.yb-anywhere.kube_config.0.cluster_ca_certificate)
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "aks-cluster" {
+  source       = "./aks-cluster"
+  cluster_name = var.cluster_name
+  region_name     = var.region_name
+  num_nodes = var.num_nodes
+}
+
+module "kubernetes-config" {
+  depends_on       = [module.aks-cluster]
+  source           = "../kubernetes-config"
+  cluster_name     = var.cluster_name
+  docker_config_json = var.docker_config_json
+}
---
+++ modules/kubernetes/aks/variables.tf
@@ -0,0 +1,17 @@
+variable "cluster_name" {
+  description = "name of the cluster to be created"
+  type = string
+  default = "yb-anywhere"
+}
+variable "region_name" {
+  description = "region to use for resources"
+  type        = string
+}
+variable "num_nodes" {
+  description = "number of nodes to create for the cluster"
+  type        = number
+}
+variable "docker_config_json" {
+  description = ".dockerconfigjson field from provided Yugabyte kubernetes secret"
+  type = string
+}
