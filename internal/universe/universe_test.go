6d62f2e4aa43035d7361f368c3b83347f97e9ffa
Steven Du <sdu@yugabyte.com>
2022-03-25 13:55:45 -0400

    [test] change network to default

--- internal/universe/universe_test.go
+++ internal/universe/universe_test.go
@@ -252,7 +252,7 @@ resource "yb_cloud_provider" "gcp" {
     { YB_FIREWALL_TAGS = "cluster-server" },
     jsondecode(file("%s"))
   )
-  dest_vpc_id = "***REMOVED***"
+  dest_vpc_id = "default"
   name        = "%s"
   regions {
     code = "us-west1"
