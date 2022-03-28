f0f58999af0167c09b2022e890b124029374cf2d
Steven Du <sdu@yugabyte.com>
2022-03-28 19:02:52 -0400

    minor

--- internal/customer/resource_customer.go
+++ internal/customer/resource_customer.go
@@ -4,11 +4,9 @@ import (
 	"context"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	client "github.com/yugabyte/platform-go-client"
 	"github.com/yugabyte/terraform-provider-yugabyte-platform/internal/api"
-	"time"
 )
 
 func ResourceCustomer() *schema.Resource {
@@ -70,14 +68,6 @@ func resourceCustomerCreate(ctx context.Context, d *schema.ResourceData, meta in
 
 	c := meta.(*api.ApiClient).YugawareClient
 
-	// if we are registering a customer, this could be a new instance of platform, so we may need to wait for startup
-	tflog.Debug(ctx, "Waiting for platform")
-	err := waitForStart(ctx, c, 10*time.Minute)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
-	tflog.Debug(ctx, "Registering customer")
 	req := client.CustomerRegisterFormData{
 		Code:     d.Get("code").(string),
 		Email:    d.Get("email").(string),
@@ -102,30 +92,6 @@ func resourceCustomerCreate(ctx context.Context, d *schema.ResourceData, meta in
 
 	d.SetId(*r.CustomerUUID)
 	return diags
-}
-
-func waitForStart(ctx context.Context, c *client.APIClient, timeout time.Duration) error {
-	wait := &resource.StateChangeConf{
-		Delay:   1 * time.Second,
-		Pending: []string{"Waiting"},
-		Target:  []string{"Ready"},
-		Timeout: timeout,
-
-		Refresh: func() (result interface{}, state string, err error) {
-			_, _, err = c.SessionManagementApi.AppVersion(ctx).Execute()
-			if err != nil {
-				return "Waiting", "Waiting", nil
-			}
-
-			return "Ready", "Ready", nil
-		},
-	}
-
-	if _, err := wait.WaitForStateContext(ctx); err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func resourceCustomerRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
