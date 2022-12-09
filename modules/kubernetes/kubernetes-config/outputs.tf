71a0af8c525e21a20ad1f275aafe4be9e68cf8be
Subramanian Neelakantan <sneelakantan@yugabyte.com>
2022-12-09 11:09:57 +0000

    Option to create ClusterIP service for yugaware UI

--- modules/kubernetes/kubernetes-config/outputs.tf
+++ modules/kubernetes/kubernetes-config/outputs.tf
@@ -1,3 +1,3 @@
 output "public_ip" {
-  value = data.kubernetes_service.yb_anywhere.status[0].load_balancer[0].ingress[0].ip
+  value = var.service_type == "ClusterIP" ? data.kubernetes_service.yb_anywhere.spec.0.cluster_ip : data.kubernetes_service.yb_anywhere.status.0.load_balancer.0.ingress.0.hostname
 }
