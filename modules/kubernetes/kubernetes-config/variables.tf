60cdc173ec364bf021cd8693b7bbafeb49dbc3bd
Subramanian Neelakantan <sneelakantan@yugabyte.com>
2022-12-09 11:09:57 +0000

    Option to create ClusterIP service for yugaware UI

--- modules/kubernetes/kubernetes-config/variables.tf
+++ modules/kubernetes/kubernetes-config/variables.tf
@@ -3,6 +3,12 @@ variable "cluster_name" {
   type = string
   default = "yb-anywhere"
 }
+
+variable "service_type" {
+  description = "Whether the Yugaware Service type should be LoadBalancer or ClusterIP"
+  type = string
+  default = "ClusterIP"
+}
 variable "docker_config_json" {
   description = ".dockerconfigjson field from provided Yugabyte kubernetes secret"
   type = string
