8e730958a854b9daf4f81a8907d44bcb3ecbba57
Deepti Kumar <108267959+Deepti-yb@users.noreply.github.com>
2023-01-05 10:25:08 +0530

    Add resource to import release versions (#59)
    * Add resource to import release versions
    
    * Applying go fmt
    
    * Applying go fmt to resource_releases

---
+++ internal/releases/package.go
@@ -0,0 +1,30 @@
+package releases
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func PackageSchema() *schema.Schema {
+	return &schema.Schema{
+		Type:     schema.TypeList,
+		ForceNew: true,
+		Optional: true,
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{
+				"path": {
+					Type:        schema.TypeString,
+					Computed:    true,
+					Optional:    true,
+					Description: "Path",
+				},
+				"arch": {
+					Type:        schema.TypeString,
+					Computed:    true,
+					Optional:    true,
+					Description: "Architecture",
+				},
+			},
+		},
+	}
+
+}
