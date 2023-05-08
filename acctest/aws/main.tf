54f63b6ea879c7d5e8773f05f7ce968465e1e49b
Deepti-yb <dkumar@yugabyte.com>
2023-05-08 06:42:13 +0000

    Acceptance testing errors
    Summary: Add change to resource in installation
    
    Test Plan: Github Actions
    
    Reviewers: vkumar, skurapati
    
    Reviewed By: skurapati
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D25123

--- acctest/aws/main.tf
+++ acctest/aws/main.tf
@@ -33,8 +33,8 @@ module "aws_yb_anywhere" {
   ssh_user            = "ubuntu"
   ssh_keypair         = "aws-acctest"
   security_group_name = "tf-acctest-sg-${random_uuid.random.result}"
-  vpc_id              = "vpc-09eea1b4c18fb9ba0"
-  subnet_id           = "subnet-0cdb90ad5eaa47ed9"
+  vpc_id              = "vpc-0fe36f6b"
+  subnet_id           = "subnet-f840ce9c"
   // files
   ssh_private_key = "${var.RESOURCES_DIR}/acctest"
 }
@@ -51,6 +51,7 @@ resource "yb_installation" "installation" {
   public_ip                 = module.aws_yb_anywhere.public_ip
   private_ip                = module.aws_yb_anywhere.private_ip
   ssh_user                  = "ubuntu"
+  ssh_host_ip               = module.aws_yb_anywhere.public_ip
   ssh_private_key           = file("${var.RESOURCES_DIR}/acctest")
   replicated_config_file    = "${var.RESOURCES_DIR}/replicated.conf"
   replicated_license_file   = "${var.RESOURCES_DIR}/acctest.rli"
