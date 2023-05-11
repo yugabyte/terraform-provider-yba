9a5d50b0b507a04efe03b5c49fc7a800e0ee35cf
Deepti-yb <dkumar@yugabyte.com>
2023-05-11 18:11:08 +0000

    License error
    Summary: -
    
    Test Plan: -
    
    Differential Revision: https://phabricator.dev.yugabyte.com/D25264

--- internal/releases/gcs.go
+++ internal/releases/gcs.go
@@ -1,7 +1,7 @@
 // Licensed to Yugabyte, Inc. under one or more contributor license
 // agreements. See the NOTICE file distributed with this work for
 // additional information regarding copyright ownership. Yugabyte
-// licenses this file to you under the Apache License, Version 2.0
+// licenses this file to you under the Mozilla License, Version 2.0
 // (the "License"); you may not use this file except in compliance
 // with the License.  You may obtain a copy of the License at
 // http://mozilla.org/MPL/2.0/.
