9cc853d4a77adc8dd1ad621e5ba38918023f7008
Etourneau Gwenn <getourneau@yugabyte.com>
2023-10-12 02:02:16 +0900

    Fix documentation for yba installer (#184)

--- examples/resources/yba_installer/resource.tf
+++ examples/resources/yba_installer/resource.tf
@@ -7,7 +7,7 @@ resource "yba_installer" "install" {
   provider                  = yba.unauthenticated
   ssh_host_ip               = "<ip-of-yba-node-for-ssh-commands>"
   ssh_user                  = "<ssh-user>"
-  ssh_private_key           = "<ssh-private-key-filepath>"
+  ssh_private_key_file_path = "<ssh-private-key-filepath>"
   yba_license_file          = "<path-to-yba-license.lic-file>"
   application_settings_file = "<path-to-application_settings.conf-file>"
   yba_version               = "<YugabyteDB Anywhere-version-with-build-number>"
